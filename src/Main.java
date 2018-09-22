93c7682ff5f8f8d0815a6be91484ab4f5f4b87ac
Przemysław Sitek <przemo2@gmail.com>
2018-09-22 12:25:30 +0200

    init

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] firstNames = new String[]{"Maciek", "Marek", "Mariusz"};
+
+        for (String firstName: firstNames
+             ) {
+            System.out.println(firstName);
+        }
+
+
+    }
+}
